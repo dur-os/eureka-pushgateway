c15d77df5f9de25c78bfc5f4499483ef480faee2
luckysnow <77icode@gmail.com>
2018-12-10 15:39:38 +0800

    feat:添加上报告检测

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@ args=""
 if [ -n "$TIMEOUT" ]; then
     args=" -timeout $TIMEOUT"
 fi
+if [ -n "$INSTANCES" ]; then
+    args=" -instances $INSTANCES"
+fi
 ./eureka-pushgateway -host $HOST_IP -eureka $EUREKA_URL -port $PORT -eport $EPORT $args
