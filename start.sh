c8e6a8e8369e0a61bee454a1a8d24d3d417dac05
77icode <77icode@gmail.com>
2018-12-18 17:18:12 +0800

    feat:添加上报告检测

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 args=""
 if [ -n "$TIMEOUT" ]; then
-    args=" -timeout $TIMEOUT"
+    args +=" -timeout $TIMEOUT"
 fi
 if [ -n "$INSTANCES" ]; then
-    args=" -instances $INSTANCES "
+    args +=" -instances $INSTANCES "
 fi
 if [ -n "$PROMETHEUS_INSTANCE" ]; then
-    args=" -prometheusInstance $PROMETHEUS_INSTANCE "
+    args +=" -prometheusInstance $PROMETHEUS_INSTANCE "
 fi
 
 ./eureka-pushgateway -host $HOST_IP -eureka $EUREKA_URL -port $PORT -eport $EPORT $args
