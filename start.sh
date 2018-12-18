14979784253ecdbdb7b4242833f740bc18dc0c6f
77icode <77icode@gmail.com>
2018-12-18 17:58:55 +0800

    feat:添加上报告检测

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-args=""
+args=
 if [ -n "$TIMEOUT" ]; then
-    args ="$args -timeout $TIMEOUT"
+    args="$args -timeout $TIMEOUT"
 fi
 if [ -n "$INSTANCES" ]; then
-    args ="$args -instances $INSTANCES "
+    args="$args -instances $INSTANCES "
 fi
 if [ -n "$PROMETHEUS_INSTANCE" ]; then
-    args ="$args -prometheusInstance $PROMETHEUS_INSTANCE "
+    args="$args -prometheusInstance $PROMETHEUS_INSTANCE "
 fi
 
 ./eureka-pushgateway -host $HOST_IP -eureka $EUREKA_URL -port $PORT -eport $EPORT $args
